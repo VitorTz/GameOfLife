6606447da11bff78d678d24d18ce39b696e5de23
VitorTz <vitor.ftz@outlook.com>
2023-02-22 11:07:15 -0300

    Update

--- src/screenshot.py
+++ src/screenshot.py
@@ -1,30 +1,48 @@
 from src.notification import ImageNotification
 from src.image import Image as GameImage
 from src.constants import Constants
+from pathlib import Path
 from time import asctime
 from PIL import Image
 
 
-def take_screenshot(image: list[list[int]]) -> None:
-    """Salva uma imagem do estado atual do jogo"""
+def mkdir_screenshot_folder() -> None:
+    """Cria a pasta de capturas de tela caso necessário"""
+    if not Constants.screenshot_dir.exists():
+        Constants.screenshot_dir.mkdir()
+    
+def get_file_name() -> Path:
+    """Cria e retorna um nome para a captura de tela"""
+    mkdir_screenshot_folder()
+    image_name = f"screenshot_{asctime()}.png"
+    return Constants.screenshot_dir / image_name
+
+
+def create_png_image(image: list[list[int]]) -> Image:
+    """Cria e retorna uma imagem para ser salva no formato .png"""
     size = (len(image[0]), len(image))
-    img = Image.new(mode="RGB", size=size, color=Constants.window_bg_color)
+    img: Image = Image.new(mode="RGB", size=size, color=Constants.window_bg_color)
     for i, line in enumerate(image):
         for j, cell in enumerate(line):
             if cell:
                 img.putpixel((j, i), value=Constants.cell_color)
-    
-    image_name = f"screenshot_{asctime()}.png"
-    if not Constants.screenshot_dir.exists():
-        Constants.screenshot_dir.mkdir()
-    f_name = Constants.screenshot_dir / image_name
-    img.save(f_name, bitmap_format='png')
+    return img
 
+def show_notification() -> None:
+    """Notifica o usuário que a captura de tela foi salva"""
     ImageNotification(
         GameImage(
             Constants.screenshot_saved_warning,
             Constants.screenshot_saved_warning_pos
         )
     ).show()
-    
+
+
+def take_screenshot(image: list[list[int]]) -> None:
+    """Salva uma imagem do estado atual do jogo"""
+    img = create_png_image(image)
+    f_name: Path = get_file_name()
+    img.save(f_name, bitmap_format='png')
+    show_notification()
+
 
